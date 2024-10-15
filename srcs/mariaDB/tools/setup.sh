9dd135a8007bf87228364f73d7b010cd663fb410
Diogo de Matos <117476220+diogodematos@users.noreply.github.com>
2024-10-15 15:19:46 +0100

    Add files via upload

---
+++ srcs/mariaDB/tools/setup.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+#start database
+service mariadb start
+
+#To test - delete after
+DB_NAME=thedatabase
+DB_USER=theuser
+DB_PASSWORD=abc
+DB_PASS_ROOT=123
+
+#Create database and users with passwords and permissions
+mariadb -v -u root << EOF
+CREATE DATABASE IF NOT EXISTS $DB_NAME;
+CREATE USER IF NOT EXISTS '$DB_USER'@'%' IDENTIFIED BY '$DB_PASSWORD';
+GRANT ALL PRIVILEGES ON $DB_NAME.* TO '$DB_USER'@'%' IDENTIFIED BY '$DB_PASSWORD';
+GRANT ALL PRIVILEGES ON $DB_NAME.* TO 'root'@'%' IDENTIFIED BY '$DB_PASS_ROOT';
+SET PASSWORD FOR 'root'@'localhost' = PASSWORD('$DB_PASS_ROOT');
+EOF
+
+#restart service
+service mariadb stop
+
+#start server
+exec mysqld_safe
