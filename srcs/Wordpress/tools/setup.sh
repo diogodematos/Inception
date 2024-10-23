f4cf56aed115446b01dc7ba76c47e49a02e00a38
Diogo de Matos <117476220+diogodematos@users.noreply.github.com>
2024-10-23 14:27:13 +0100

    Add files via upload

--- srcs/Wordpress/tools/setup.sh
+++ srcs/Wordpress/tools/setup.sh
@@ -1,8 +1,11 @@
-#Change the owner of the wordpress files to www-data user.
-chown -R www-data:www-data /var/www/html/wordpress
+#!/bin/bash
 
-#Move the wp-config.php file to the root of the wordpress files.
-mv /var/www/html/wordpress/wp-config.php /var/www/html/wp-config.php
+sleep 2s
+
+cd /var/www/wordpress
+
+if [ ! -f wp-config.php ]; then
+    echo "'wp-config.php' not found. Generating wp-config.php..."
 
 #Create the temp var to be used now, but at the end, we'll have a .env file with all the variables. So you need to remove these declaration lines.
 WP_URL=login.42.fr
@@ -15,34 +18,39 @@ WP_PASSWORD=abc
 WP_EMAIL=theuser@123.com
 WP_ROLE=editor
 
-#Download the wordpress files
-sleep 10
-wp --allow-root --path="/var/www/inception/" core download || true
+else
+    echo "'wp-config.php' already exists, skipping config creation."
+fi
 
-#If the wordpress files aren't there, create install wordpress and set it, if not move foward
-if ! wp --allow-root --path="/var/www/inception/" core is-installed;
-then
-    wp  --allow-root --path="/var/www/inception/" core install \
-        --url=$WP_URL \
-        --title=$WP_TITLE \
+if ! wp core is-installed --allow-root; then
+    echo "WordPress not detected, installing WordPress..."
+
+    wp core install --allow-root \
+        --url=https://$WP_DOMAIN \
+        --title="$WP_TITLE" \
         --admin_user=$WP_ADMIN_USER \
         --admin_password=$WP_ADMIN_PASSWORD \
         --admin_email=$WP_ADMIN_EMAIL
-fi;
 
-#Create the non-admin user and set its role.
-if ! wp --allow-root --path="/var/www/inception/" user get $WP_USER;
-then
-    wp  --allow-root --path="/var/www/inception/" user create \
-        $WP_USER \
-        $WP_EMAIL \
-        --user_pass=$WP_PASSWORD \
-        --role=$WP_ROLE
-fi;
+    echo "WordPress installation complete."
+else
+    echo "WordPress is already installed, skipping installation."
+fi
 
-#Download another theme and activate it. It's not necessary, but I did because I don't like the default theme.
-wp --allow-root --path="/var/www/inception/" theme install raft --activate 
+if ! wp user get $WP_GUEST_USER --allow-root > /dev/null 2>&1; then
+    wp user create --allow-root \
+        $WP_GUEST_USER \
+        $WP_GUEST_EMAIL \
+        --role=author \
+        --user_pass=$WP_GUEST_PASSWORD
+    echo "Guest user created."
+else
+    echo "Guest user '$WP_GUEST_USER' already exists, skipping user creation."
+fi
 
-#start the php server in foreground
-exec $@
+echo "Starting PHP-FPM in foreground..."
+
+service php7.4-fpm start 
+service php7.4-fpm stop
+/usr/sbin/php-fpm7.4 -F 
 
